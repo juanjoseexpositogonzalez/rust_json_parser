e1f83dc8b2c5193512dc1a825ce7dfa559ca9fe3
Juanjo <expositogonzalez.juanjose@gmail.com>
2025-11-05 18:26:12 +0100

    Finished tokenize function and Token enum. All tests pass

---
+++ target/debug/deps/rust_json_parser-5e09d1e39691cc78.d
@@ -0,0 +1,8 @@
+/home/juanjo/code/app/rust_json_parser/target/debug/deps/rust_json_parser-5e09d1e39691cc78.d: src/lib.rs src/tokenizers.rs
+
+/home/juanjo/code/app/rust_json_parser/target/debug/deps/librust_json_parser-5e09d1e39691cc78.rlib: src/lib.rs src/tokenizers.rs
+
+/home/juanjo/code/app/rust_json_parser/target/debug/deps/librust_json_parser-5e09d1e39691cc78.rmeta: src/lib.rs src/tokenizers.rs
+
+src/lib.rs:
+src/tokenizers.rs:
---
+++ target/debug/deps/rust_json_parser-a6b8aed8cb34bd44.d
@@ -0,0 +1,5 @@
+/home/juanjo/code/app/rust_json_parser/target/debug/deps/rust_json_parser-a6b8aed8cb34bd44.d: src/main.rs
+
+/home/juanjo/code/app/rust_json_parser/target/debug/deps/librust_json_parser-a6b8aed8cb34bd44.rmeta: src/main.rs
+
+src/main.rs:
